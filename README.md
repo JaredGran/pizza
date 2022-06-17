ade796a8c3bdb6e9000e434638fc7e1af36a8b87
JaredGran <jared.carper@gmail.com>
2022-06-17 10:35:59 -0700

    ^

---
+++ README.md
@@ -0,0 +1,2 @@
+details: function Pizza(toppings)
+test: "creat object that can be called on to make a new pizza"
---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function Pizza(topping1, topping2, topping3) {
+  this.toppingOne = topping1;
+  this.toppingTwo = topping2;
+  this.toppingThree = topping3;
+}
