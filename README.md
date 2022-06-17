37f42d4baf908b61d7869805ad967cc9dde21c1f
JaredGran <jared.carper@gmail.com>
2022-06-17 14:15:19 -0700

    forgot to write down tests so heres one

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-describe:function Pizza(toppings)
+describe:function PizzaOrder(orderName, orderId, type, Price)
 test: "creat object that can be called on to make a new pizza"
+Code:function PizzaOrder(orderName, orderId, type, Price)
+  this.orderName = orderName;
+  this.orderId = orderId
+  this.orderNum = orderNum;
+  this.type = type;
+  this.price = Price;
+}
+pizza = new pizzaOrder("mark","seventeen", "supreme", '19.50')
+Expected Output: pizza (mark, seventeen, supreme, 19.50)
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-function Pizza(topping1, topping2, topping3) {
-  this.toppingOne = topping1;
-  this.toppingTwo = topping2;
-  this.toppingThree = topping3;
+var orderNum = 0
+var type = new Array["plain", "supreme", "meat lovers"]
+function PizzaOrder(orderName, orderId, type, Price) {
+  this.orderName = orderName;
+  this.orderId = orderId
+  this.orderNum = orderNum;
+  this.type = type;
+  this.price = Price;
 }
+
+PizzaOrder.prototype.give = function(type, orderName){
+  orderTag = orderNum + 1;
+
+  if(type === "plain"){
+    price = 10
+}
+  else if(type === "supreme"){
+    price = 18
+  }
+  else(){
+    price === 25
+  }
+  print
+}
+
